87f68d81d86d84df61d5517f4f595f4dc23e6b57
ItzHefe <jeffrey.jarvis32@gmail.com>
2022-01-13 22:46:44 -0500

    added in prompts on button click. added in different rules variables. new vars are not connected to prompts

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,6 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var genPW = document.getElementById("password");
+var upperSet = ("QAZWSXEDCRFVTGBYHNUJMIKLOP");
+var lowerSet = ("qazwsxedcrfvtgbyhnujmiklop");
+var numberSet = ("123456789");
+var specialSet = ("!@#$%^&*()");
 
 function generatePassword () {
   
@@ -20,13 +24,25 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+document.querySelector('#generate').addEventListener('click', promptMe);
 
+function promptMe() {
+  var selectUpper = window.prompt ("Include uppercase? Type Y or Yes");
+  var selectLower = window.prompt ("Include lowercase? Type Y or Yes");
+  var selectNumber = window.prompt ("Include numbers? Type Y or Yes");
+  var selectSpecial = window.prompt ("Include Characters? Type Y or Yes");
+}
+
+selectUpper = selectUpper.toUpperCase();
+selectLower = selectLower.toUpperCase();
+selectNumber = selectNumber.toUpperCase();
+selectSpecial = selectSpecial.toUpperCase();
 
 
 
